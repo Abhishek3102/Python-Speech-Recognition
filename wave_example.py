eb893fc44bab72dd8eefc98a24b1d105d19c503f
Abhishe16 <ankushpan694@gmail.com>
2024-06-03 11:27:43 +0530

    more details of wav audio file

--- wave_example.py
+++ wave_example.py
@@ -7,3 +7,10 @@ print("Sample width :", obj.getsampwidth())
 print("Frame Rate :", obj.getframerate())
 print("Nnumber of frames :", obj.getnframes())
 print("Parameters :", obj.getparams())
+
+time_of_audio = obj.getnframes() / obj.getframerate()
+print("The time of audio in seconds is :", time_of_audio)
+
+frames = obj.readframes(-1)
+print(type(frames), type(frames[0]))
+print(len(frames))
