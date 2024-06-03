ae2c1edae8a07de37371f10dd549eb782d28625d
Abhishe16 <ankushpan694@gmail.com>
2024-06-03 11:52:57 +0530

    made a plot of audio

--- plot_audio.py
+++ plot_audio.py
@@ -12,3 +12,15 @@ obj.close()
 
 audio_time = n_samples / sample_frequency
 print("The time of audio in seconds is :", audio_time)
+
+signal_array = np.frombuffer(signal_wave, dtype=np.int16)
+
+times = np.linspace(0, audio_time, num = n_samples)
+
+plt.figure(figsize=(15,5))
+plt.plot(times, signal_array)
+plt.title("Audio signal")
+plt.ylabel("Signal Wave")
+plt.xlabel("Time (s)")
+plt.xlim(0, audio_time)
+plt.show()
