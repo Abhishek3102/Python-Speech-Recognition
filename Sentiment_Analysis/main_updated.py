31425c7203721cdced3fdbc1bf541242c11b5816
Abhishe16 <ankushpan694@gmail.com>
2024-06-05 14:49:55 +0530

    changes made

---
+++ Sentiment_Analysis/main_updated.py
@@ -0,0 +1,20 @@
+import yt_dlp
+ydl = yt_dlp.YoutubeDL()
+
+def get_video_info(url):
+    with ydl:
+        result = ydl.extract_info(
+            url,
+            download=False
+        )
+    if "entries" in result:
+        return result["entries"][0]
+    return result
+
+def get_audio_url(video_info):
+    print(video_info)
+
+if __name__ == "__main__":
+    video_info = get_video_info("https://youtu.be/MD0k7aWS5Fc?si=VW1dm4s8A4sWU9yN")
+    audio_url = get_audio_url(video_info)
+    print(audio_url)
