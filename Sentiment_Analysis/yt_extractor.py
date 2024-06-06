d55380120d47e642cc7bd1508dea056643faeb0e
Abhishe16 <ankushpan694@gmail.com>
2024-06-06 11:36:17 +0530

    added

--- Sentiment_Analysis/yt_extractor.py
+++ Sentiment_Analysis/yt_extractor.py
@@ -1,23 +1,34 @@
-import yt_dlp as youtube_dl
+import yt_dlp
+from yt_dlp.utils import DownloadError
 
-ydl = youtube_dl.YoutubeDL()
+ydl = yt_dlp.YoutubeDL()
 
 def get_video_info(url):
     with ydl:
-        result = ydl.extract_info(
-            url,
-            download=False
-        )
-    if "entries" in result:
-        return result["entries"][0]
-    return result
+        try:
+            result = ydl.extract_info(
+                url,
+                download=False
+            )
+        except DownloadError:
+            return None
 
-def get_audio_url(video_info):
-    for f in video_info["formats"]:
-        if f["ext"] == "m4a":
-            return f["url"]
+    if 'entries' in result:
+        # Can be a playlist or a list of videos
+        video = result['entries'][0]
+    else:
+        # Just a video
+        video = result
+    return video
 
-if __name__ == "__main__":
+
+def get_audio_url(video):
+    for f in video['formats']:
+        if f['ext'] == 'm4a':
+            return f['url']
+    
+
+if __name__ == '__main__':
     video_info = get_video_info("https://youtu.be/MD0k7aWS5Fc?si=VW1dm4s8A4sWU9yN")
-    audio_url = get_audio_url(video_info)
-    print(audio_url)
+    url = get_audio_url(video_info)
+    print(url)
